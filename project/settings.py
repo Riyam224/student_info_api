bba121fec340c2e3dba5409b7c7d1ef65fde2dfb
Riyam224 <cs50riyam211#gmail.com>
2024-01-08 15:54:20 +0300

    add some settings and static files

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6m)()ewj=n)8bbw2ht=dm9b0b(@i$%2*@z+d4xertlbk%=po4+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "yoora.pythonanywhere.com"]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
