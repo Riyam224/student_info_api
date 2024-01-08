d4657804ed704b801079e923158aa2840a45d323
Riyam224 <cs50riyam211#gmail.com>
2024-01-08 15:35:01 +0300

    edit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6m)()ewj=n)8bbw2ht=dm9b0b(@i$%2*@z+d4xertlbk%=po4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
